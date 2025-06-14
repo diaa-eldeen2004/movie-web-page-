e0615273c7acd414d1a6854e8f738efac967de10
Diaa Eldeen <dodgeldeen@gmail.com>
2025-06-14 06:51:15 +0300

    Delete routes directory

--- routes/contact.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { sendContactMessage } = require("../controllers/contact");
-
-// Public route
-router.post("/", sendContactMessage);
-
-module.exports = router; 
